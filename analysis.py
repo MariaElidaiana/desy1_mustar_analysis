fixing deltasigma NFW units, units and mcmc start position

analysis.py
@@ -35,7 +35,7 @@ def find_best_fit(args):
     return
 
 def do_mcmc(args):
-    nwalkers, nsteps = 32,5000
+    nwalkers, nsteps = 32, 1500
     runtype = args['runtype']
     bfpath = args['bf_file']
     chainpath = args['chainfile']
@@ -44,41 +44,30 @@ def do_mcmc(args):
     bfmodel = np.loadtxt(bfpath) #Has everything
     start = get_mcmc_start(bfmodel, runtype)
     ndim = len(start) #number of free parameters
-
-    priors = ([1e12, 1e15])
-
-    # Initialize m200, p_cen, B0 and Rs at the center of prior:
-    mid = np.zeros(ndim)
-    for i in range(ndim):
-        mid[i] = (priors[2*i] + priors[2*i + 1])
-    mid *= 0.5
-
-    zeros = np.zeros((ndim, nwalkers))
-    np.random.seed(0)
-    pos=[]
-    for k in range(ndim):
-        zeros[k,:] = mid[k] + 1e-1 * np.random.normal(loc=0.0, scale=np.abs(mid[k]), size=nwalkers)
-
-    for i in range(nwalkers):
-        if runtype=='data':
-            pos.append(np.array([zeros[0,i],zeros[1,i], zeros[2,i],zeros[3,i]]))
-        elif runtype=='cal':
-            pos.append(np.array([zeros[0,i]]))
-
     print 'ndim', ndim
     #pos = [start + 1e-3*np.random.randn(ndim) for k in range(nwalkers)]
-    print 'pos', pos
+    pos = [ np.array(start) * 1.e-1 * abs(np.random.randn((ndim))) for k in range(nwalkers)] # work around
+    '''
+    h0 = 1e-1
+    np.random.seed(0)
+    pos=[]
+    zeros = np.zeros((ndim, nwalkers))
+    for k in range(len(start)):
+        zeros[k,:] = start[k] + h0 * np.random.normal(loc=0.0, scale=np.abs(start[k]), size=nwalkers)
 
-
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnposterior, args=(args,), threads=8)
+    for i in range(nwalkers):
+        pos.append(np.array([zeros[0,i]]))
+    '''
+    print 'Start point for mcmc : ', pos[0]
+    print pos
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnposterior, args=(args,), a=6, threads=8)
     print "Starting MCMC, saving to %s"%chainpath
     sampler.run_mcmc(pos, nsteps)
     print "MCMC complete"
     print "Mean acceptance fraction: %.3f" % (np.mean(sampler.acceptance_fraction))
     np.save(chainpath, sampler.chain)
     np.save(likespath, sampler.lnprobability)
-    #np.savetxt(chainpath, sampler.flatchain)
-    #np.savetxt(likespath, sampler.flatlnprobability)
+    sampler.reset()
     return
 
 

helper.py
@@ -15,7 +15,7 @@ from colossus.halo import concentration
 from colossus.cosmology import cosmology
 from scipy.interpolate import interp2d
 
-calpath    = '/Users/maria/current-work/maria_wlcode/Fox_Sims/DeltaSigmas/'
+calpath    = '/Users/maria/current-work/maria_wlcode/Fox_Sims/data_files/'
 y1datapath = '/Users/maria/current-work/lambda_star/connor-data-final/runs_Feb19/DES_mu_star_wlmass/results_v2/'
 samplepath = '/Users/maria/current-work/lambda_star/connor-data-final/runs_Feb19/DES_mu_star_samples/'
 bfpath     = '/Users/maria/current-work/maria_wlcode/Fox_Sims/'
@@ -248,8 +248,8 @@ def get_data_and_icov(dsfile, dscovfile, runtype):
         print "Calibration file: ", dsfile
         calibpath = calpath + dsfile
 
-        R0, ds0 = np.genfromtxt(calibpath, unpack=True) #comoving
-        print 'All data:\n', np.c_[R0,ds0]
+        R0, ds0 = np.genfromtxt(calibpath, unpack=True) #comoving [Mpc/h], [hMsun/pc^2]
+        print 'All data (in comoving [Mpc/h],[hMsun/pc2]):\n', np.c_[R0,ds0]
 
         mubin = dsfile.split('_')[2].split('.')[0][1]
         print "mu_star bin = ", mubin
@@ -275,24 +275,25 @@ def get_data_and_icov(dsfile, dscovfile, runtype):
 
         print "z mean = ", z_mean
 
+        if mubin=='0':
+            fitmask = (R0>=0.3)&(R0<=2.5) #mu1
+        elif mubin=='1':
+            fitmask = (R0>=0.3)&(R0<=3.5) #mu2
+        elif mubin=='2':
+            fitmask = (R0>=0.3)&(R0<=3.5) #mu3
+        elif mubin=='3':
+            fitmask = (R0>=0.3)&(R0<=2.5) #mu4
+
+        R = R0[fitmask]   #physical [Mpc]
+        ds = ds0[fitmask] #physical [Msun/pc^2]
+
         #The simulations are in comoving distances.
         #I need to convert radius and deltasigma from comoving to physical.
         h=cosmology['h']
-        R0 = R0/h*(1+z_mean)       #comoving to physical
-        ds0 = ds0*h*(1+z_mean)**2  #comoving to physical
+        R0 = R0/h*(1+z_mean)       #comoving [Mpc/h] to physical [Mpc]
+        ds0 = ds0/h*(1+z_mean)**2  #comoving [hMsun/pc^2] to physical [Msun/pc^2]
 
-        if mubin=='0':
-            fitmask = (R0>=0.3)&(R0<=2.5)                #mu1
-        elif mubin=='1':
-            fitmask = (R0>=0.3)&(R0<=2.8617856063833296) #mu2
-        elif mubin=='2':
-            fitmask = (R0>=0.3)&(R0<=3.149802624737183)  #mu3
-        elif mubin=='3':
-            fitmask = (R0>=0.3)&(R0<=3.502549163319234)  #mu4
-
-        R = R0[fitmask]   #physical
-        ds = ds0[fitmask] #physical
-        print 'Sim-data after radial cut for the mcmc:\n', np.c_[R, ds]
+        print 'Sim-data after radial cut (in physical [Mpc],[Msun/pc2]) for the mcmc:\n', np.c_[R, ds]
 
         #The simulations don't have errors. The covariances for the simulated
         #data have to be the data covariances. That's why I getting them here.
@@ -302,14 +303,16 @@ def get_data_and_icov(dsfile, dscovfile, runtype):
         cov = np.genfromtxt(covpath)
 
         cov = cov[fitmask]
-        cov = cov[:,fitmask]
-        print 'Data after radial cut for the mcmc:\n', np.c_[R, ds]
+        #The data cov is in units of physical [hMsun/pc^2]^2
+        #Have to multiply by h to be in units of physical [Msun/pc^2]^2
+        cov = cov[:,fitmask]*h
+
         #Apply the Hartlap correction, to get an unbiased cov matrix estimator.
         #It boosts the covariance values.
         Njk = 100.
         D = len(R)
-        cov = cov*((Njk-1.)/(Njk-D-2))  #physical
-        icov = np.linalg.inv(cov)       #physical
+        cov = cov*((Njk-1.)/(Njk-D-2))  #physical [Msun/pc^2]^2
+        icov = np.linalg.inv(cov)       #physical [Msun/pc^2]^2
 
     return R, ds, icov, cov, fitmask
 
@@ -326,7 +329,7 @@ def get_mcmc_start(model, runtype):
 
 def get_model_defaults(h):
     #Dictionary of default starting points for the best fit
-    defaults = {'m200': 0.9e14, #m200 is in Msun/h
+    defaults = {'m200': 1e14, #m200 is in Msun/h
                 'pcc' : 0.75,
                 'B0'  : 0.10,
                 'Rs'  : 2.50} #Mpc/h physical

likelihoods.py
@@ -13,10 +13,14 @@ from models import *
 def lnlikelihood(theta, args):
     h=args['h']
     z_mean = args['z_mean']
-    DSobs = args['DS']              #physical
-    iDScov = args['icov']           #physical
-    DSmod = DStheo(theta, args)     #comoving
-    DSmod *= h*(1+z_mean)**2        #physical
+    DSobs = args['DS']             #physical [Msun/pc^2]
+    iDScov = args['icov']          #physical [Msun/pc^2]
+
+    DSobs = DSobs*(1+z_mean)**2    #physical [Msun/pc^2] to comoving [Msun/pc^2]
+    iDScov = iDScov*(1+z_mean)**2  #physical [Msun/pc^2] to comoving [Msun/pc^2]
+
+    DSmod = DStheo(theta, args)    #comoving [Msun/pc^2]
+    #DSmod /= (1+z_mean)**2        #physical [Msun/pc^2]
     DSdiff = DSobs - DSmod
     lnlikeDS = -0.5*np.dot(DSdiff, np.dot(iDScov, DSdiff))
     return lnlikeDS