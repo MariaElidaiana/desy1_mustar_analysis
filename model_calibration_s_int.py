1f4a71ab8f0ddd5007d2f36a0ca228a52d82b16c
MariaElidaiana <elidaiana.sp@gmail.com>
2019-07-03 15:11:14 -0400

    Final version of the model systematics scripts

--- model_calibration_s_int.py
+++ model_calibration_s_int.py
@@ -6,32 +6,11 @@ import emcee
 import scipy.optimize as op
 import corner
 import matplotlib.pyplot as plt
-
-simsdir = "/Users/maria/current-work/maria_wlcode/Fox_Sims/"
-Msim    = np.genfromtxt(simsdir + "mustar_ps25_masses_crit_diemer18.txt")              #M200c [Mpc] converted from sims
-mu      = np.genfromtxt(simsdir + "better_simulated_profiles/mustar_ps25_mustars.txt") #mu_star [Msun] from sims
-M       = np.genfromtxt(simsdir + "mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18/diemer18_sims_masses_crit.txt")    #M200c [Msun]
-Me      = np.genfromtxt(simsdir + "mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18/diemer18_sims_err_masses_crit.txt") #M200c [Msun]
-
-C = Msim/M    #C=M_true/M_obs
-Ce = C*(Me/M) #or (M_true*Me)/M**2
-
-zs = [1.0, 0.5, 0.25, 0.0]
-z = np.copy(zs)
-for i in range(3):
-    z = np.vstack((z, zs))
-z = np.array(z).T
-
-print '-'*30, 'z'
-print z
-print '-'*30, 'C'
-print C
-print '-'*30, 'Cerr'
-print Ce
-print '-'*30
-
-zpivot = 1.5       #zpivot = (1+z0) = (1+0.5)
-mupivot = 5.16e+12 #Same pivot as the previous SDSS mu_star calibration
+import matplotlib
+matplotlib.rc('xtick', labelsize=12)
+matplotlib.rc('ytick', labelsize=12)
+plt.rc('text', usetex=True)
+ORG = plt.get_cmap('OrRd')
 
 def get_C_model(params, mu, z):
     C0, a, b, lnf = params
@@ -59,72 +38,132 @@ def lnprob(params, C, Cerr, mu, z):
     if not np.isfinite(lnp): return -np.inf
     return lnp + lnlike(params, C, Cerr, mu, z)
 
-#Minimizer step
-lnprobargs = (C, Ce, mu, z)
-nll = lambda *args: -lnprob(*args)
-result = op.minimize(nll, [1.04, 0.03, 0.03, 0.], args=lnprobargs)
-print result
+#Loading the data
+simsdir = "/Users/maria/current-work/maria_wlcode/Fox_Sims/"
+
+Msim    = np.genfromtxt(simsdir + "mustar_ps25_masses_crit_diemer18.txt")              #M200c [Mpc] converted from sims
+mu      = np.genfromtxt(simsdir + "better_simulated_profiles/mustar_ps25_mustars.txt") #mu_star [Msun] from sims
+M       = np.genfromtxt(simsdir + "mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18_corr/diemer18_sims_masses_crit.txt")     #M200c [Msun]
+Me      = np.genfromtxt(simsdir + "mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18_corr/diemer18_sims_err_masses_crit.txt") #M200c [Msun]
+
+C = Msim/M    #C=M_true/M_obs
+Ce = C*(Me/M)
+
+zs = np.array([1.0, 0.5, 0.25, 0.0])
+z = np.copy(zs)
+for i in range(3):
+    z = np.vstack((z, zs))
+z = np.array(z).T
+
+zpivot = 1.5       #zpivot = (1+z0) = (1+0.5)
+mupivot = 5.16e+12 #Same pivot as the previous SDSS mu_star calibration
 
 #Mcmc configuration
 ndim = 4
 nwalkers = 32
 nsteps = 10000
+
+#Minimizer step
+lnprobargs = (C, Ce, mu, z)
+nll        = lambda *args: -lnprob(*args)
+result     = op.minimize(nll, [1.04, 0.03, 0.025, np.log(0.01)], args=lnprobargs)
+print result
+
 sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=lnprobargs)
 pos = [result["x"] + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
 sampler.run_mcmc(pos, nsteps)
 chain = sampler.flatchain
-np.savetxt("./mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18/calchain_s_int_range0-2_2-5.txt", chain) #Chains are saved without remove burn-in
-chainpath = "./mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18/calchain_s_int_range0-2_2-5.npy"
+
+np.savetxt("./mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18_corr/calchain_s_int_range0-2_2-5.txt", chain) #Chains are saved without remove burn-in
+chainpath = "./mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18_corr/calchain_s_int_range0-2_2-5.npy"
 np.save(chainpath, sampler.chain)
 
 #Read the flatchain format
 chain = np.load(chainpath)
 
 #Apply burn-in
-burn_in = 3000
-chain = chain[:, burn_in:, :]
+burn_in = 2000
+chain   = chain[:, burn_in:, :]
 samples = chain.reshape((-1, chain.shape[2]))
 
-print '-'*30
-print 'Burn-in=', burn_in
 means = np.array([ np.mean(samples[:, 0]), np.mean(samples[:, 1]), np.mean(samples[:, 2]), np.mean(samples[:, 3]) ])
 stds  = np.array([ np.std(samples[:, 0]), np.std(samples[:, 1]), np.std(samples[:, 2]), np.std(samples[:, 3]) ])
 
 print 'means C0 a b ln(V) = ', means
-print 'errors C0 a b ln(V) = ',stds
-print
+print 'errors C0 a b ln(V) = ', stds
 print 'mean V =', np.exp(means[3])
 print 'error V=', np.abs((np.exp(means[3])/means[3]) * stds[3])
-print
 print 'sqrt(mean V) = sigma_int = ', np.sqrt(np.exp(means[3]))
-print '-'*30
 
 #Plotting corner
-samples = sampler.chain[:, burn_in:, :].reshape((-1, ndim))
-
 #Plot ln(V) = ln(sigma_int^2)
 c0_mcmc, a_mcmc, b_mcmc, lnV_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
                              zip(*np.percentile(samples, [16, 50, 84],
                                                 axis=0)))
-rlims = [[0.85, 1.05], [-0.15, 0.186], [-0.5, 0.15], [-13., -2.5]]
+bestfit_values = [c0_mcmc[0], a_mcmc[0], b_mcmc[0], lnV_mcmc[0]]
 fig = corner.corner(samples, labels=['$C_0$',r'$\alpha$', r'$\beta$', '$\ln\,\sigma_{\mathcal{C} }^2$'],
-                    smooth=1.75, show_titles=False, range=rlims, title_fmt='.3f',
-                    label_kwargs=dict(fontsize=20), title_kwargs=dict(fontsize=15), use_math_text=True)
+                    bins=40, smooth=2,
+                    show_titles=True,
+                    quantiles=[0.16, 0.5, 0.84],
+                    color=ORG(0.7),
+                    levels=[0.16, 0.50, 0.84],
+                    label_kwargs=dict(fontsize=20),
+                    title_kwargs=dict(fontsize=15),
+                    plot_contours=True,
+                    fill_contours=True,
+                    hist_kwargs={"histtype": 'stepfilled', "alpha": 0.5,
+                    "edgecolor": "none"},
+                    use_math_text=True, truths=bestfit_values, truth_color='skyblue',
+                    **{'title_fmt':'.3f', 'plot_datapoints': False})
+
 for ax in fig.get_axes():
-    #ax.tick_params(axis='both', which='major', labelsize=14)
-    #ax.tick_params(axis='both', which='minor', labelsize=12)
     ax.tick_params(axis='both', labelsize=14)
-plt.savefig('./mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18/C_calibration_with_intrinsic_scatter_lnV_range0-2_2-5.png')
+plt.savefig('./mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18_corr/C_calibration_with_intrinsic_scatter_lnV_range0-2_2-5_colored.png', bbox_inches="tight")
 
 #Plot V
 samples[:, 3] = np.sqrt(np.exp(samples[:, 3])) #sqrt(exp(\ln sigma_int^{2}))
 c0_mcmc, a_mcmc, b_mcmc, f_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
                                       zip(*np.percentile(samples, [16, 50, 84],
                                                          axis=0)))
-rlims = [[0.85, 1.05], [-0.15, 0.186], [-0.5, 0.15], [-0.03, 0.1]]
+
+bestfit_values = [c0_mcmc[0], a_mcmc[0], b_mcmc[0], f_mcmc[0]]
+rlims = [[0.9, 1.05], [-0.085, 0.17], [-0.45, 0.0], [-0.01,0.06]]
 fig = corner.corner(samples, labels=['$C_0$',r'$\alpha$', r'$\beta$', '$\sigma_{\mathcal{C}}$'],
-                    smooth=1.75, show_titles=False, range=rlims,  title_fmt='.3f',
-                    label_kwargs=dict(fontsize=20), title_kwargs=dict(fontsize=17), use_math_text=True)
+                    bins=40, smooth=2,
+                    show_titles=True,
+                    range=rlims,
+                    quantiles=[0.16, 0.5, 0.84],
+                    color=ORG(0.7),
+                    levels=[0.16, 0.50, 0.84],
+                    label_kwargs=dict(fontsize=26),
+                    title_kwargs=dict(fontsize=20),
+                    plot_contours=True,
+                    fill_contours=True,
+                    hist_kwargs={"histtype": 'stepfilled', "alpha": 0.5,
+                    "edgecolor": "none"},
+                    use_math_text=True, truths=bestfit_values, truth_color='skyblue',
+                    **{'title_fmt':'.3f', 'plot_datapoints': False})
+
 for ax in fig.get_axes():
     ax.tick_params(axis='both', labelsize=14)
-plt.savefig('./mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18/C_calibration_with_intrinsic_scatter_f_range0-2_2-5.png')
+plt.savefig('./mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18_corr/C_calibration_with_intrinsic_scatter_f_range0-2_2-5_colored.png', bbox_inches="tight")
+
+#without titles
+fig = corner.corner(samples, labels=['$C_0$',r'$\alpha$', r'$\beta$', '$\sigma_{\mathcal{C}}$'],
+                    bins=40, smooth=2,
+                    show_titles=False,
+                    range=rlims,
+                    quantiles=[0.16, 0.5, 0.84],
+                    color=ORG(0.7),
+                    levels=[0.16, 0.50, 0.84],
+                    label_kwargs=dict(fontsize=20),
+                    title_kwargs=dict(fontsize=17),
+                    plot_contours=True,
+                    fill_contours=True,
+                    hist_kwargs={"histtype": 'stepfilled', "alpha": 0.5,
+                    "edgecolor": "none"},
+                    use_math_text=True, truths=bestfit_values, truth_color='skyblue',
+                    **{'title_fmt':'.3f', 'plot_datapoints': False})
+for ax in fig.get_axes():
+    ax.tick_params(axis='both', labelsize=14)
+plt.savefig('./mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18_corr/C_calibration_with_intrinsic_scatter_f_range0-2_2-5_notitle_colored.png', bbox_inches="tight")
--- see_calibration.py
+++ see_calibration.py
@@ -1,6 +1,5 @@
 """
 See the calibration curves.
-Note: we only have best fit masses so far.
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,28 +8,12 @@ from matplotlib.ticker import MaxNLocator
 plt.rc("text", usetex=True, fontsize=24)
 plt.rc("errorbar", capsize=3)
 
-use_SigmaInt = True
-
-mass = np.loadtxt("/Users/maria/current-work/maria_wlcode/Fox_Sims/mustar_ps25_masses_crit_diemer18.txt")
-
-if use_SigmaInt:
-    bfs  = np.loadtxt("/Users/maria/current-work/maria_wlcode/Fox_Sims/mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18/diemer18_sims_masses_crit.txt")
-    errs = 0.015
-else:
-    bfs  = np.loadtxt("/Users/maria/current-work/maria_wlcode/Fox_Sims/mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18/diemer18_sims_masses_crit.txt")
-    errs = np.loadtxt("/Users/maria/current-work/maria_wlcode/Fox_Sims/mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18/diemer18_sims_err_masses_crit.txt")
-
-C = mass/bfs
-if not use_SigmaInt:
-    Cerr = C * (errs/bfs) # the same as Cerr = mass*(errs/bfs**2)
-
-mus = np.loadtxt("/Users/maria/current-work/maria_wlcode/Fox_Sims/better_simulated_profiles/mustar_ps25_mustars.txt")
-zs = [1.0, 0.5, 0.25, 0.0]
-
 def calc_cal(mu, z):
-    ## Results with intrinsic scatter, for radial bins 0.2 to 2.5:
-    C0, a , b   = [ 0.95455359, 0.03090107, -0.18034199]  #diemer18
-    dC0, da, db = [0.02349054, 0.04583875, 0.06991657]    #diemer18
+    #C0, a , b   = [ 0.95455359, 0.03090107, -0.18034199]  #diemer18
+    #dC0, da, db = [0.02349054, 0.04583875, 0.06991657]    #diemer18
+    C0, a , b   = [0.97767676,  0.0424727, -0.23112437]   #diemer18 + sys
+    dC0, da, db = [0.02925375, 0.05641144, 0.09145634]    #diemer18 + sys
+
     Cbf = C0 * (mu/5.16e12)**a * ((1+z)/1.5)**b
     
     if use_SigmaInt:
@@ -50,33 +33,52 @@ def get_cal_curve(z):
     Cbf, Cbfe = calc_cal(mu, z)
     return [mu, Cbf, Cbfe]
 
+#Load the data
+use_SigmaInt = True
 color = ['firebrick', 'tomato', 'royalblue', 'darkslateblue']
 
-fig, axarr = plt.subplots(4, sharex=True, sharey=True)
+mariapath = "/Users/maria/current-work/maria_wlcode/Fox_Sims/"
+mass = np.loadtxt(mariapath + "mustar_ps25_masses_crit_diemer18.txt")
+
+if use_SigmaInt:
+    bfs  = np.loadtxt(mariapath + "mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18_corr/diemer18_sims_masses_crit.txt")
+    errs = 0.0166
+else:
+    bfs  = np.loadtxt(mariapath + "mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18_corr/diemer18_sims_masses_crit.txt")
+    errs = np.loadtxt(mariapath + "mcmc_fox_calibration/mcmc_fox_calibration_1e11M1e18_diemer18_corr/diemer18_sims_err_masses_crit.txt")
+
+C = mass/bfs
+if not use_SigmaInt:
+    Cerr = C * (errs/bfs) # the same as Cerr = mass*(errs/bfs**2)
+
+mus = np.loadtxt(mariapath + "better_simulated_profiles/mustar_ps25_mustars.txt")
+zs = [1.0, 0.5, 0.25, 0.0]
+
+#Do the plot
+fig, axarr = plt.subplots(4, sharex=True)
 for i in range(len(zs)):
     z = zs[i]
     hi = mus[i]>0.
     if use_SigmaInt:
         axarr[i].errorbar(mus[i,hi]/1.e12, C[i,hi], errs, marker='.', ls='', c=color[i])
+        print 'C-data = ', C[i,hi]
     else:
         axarr[i].errorbar(mus[i,hi]/1.e12, C[i,hi], Cerr[i,hi], marker='o', ls='', c=color[i])
 
     print calc_cal(mus[i,hi], z)[0]
     print calc_cal(mus[i,hi], z)[1]
-    print
+
     mux, Cbf, Cbfe = get_cal_curve(z)
     axarr[i].fill_between(mux/1.e12, Cbf-Cbfe, Cbf+Cbfe, color=color[i],alpha=0.2, zorder=-1)
     axarr[i].axhline(y=1.0, c='k', ls='--', lw=1)
     axarr[i].plot(mux/1.e12, Cbf, color=color[i], ls='-')
 
-for i in range(len(zs)):
     ylim = .32
-    #axarr[i].set_ylim(0.7, 1.3)
-    axarr[i].set_ylim(0.85, 1.15)
     axarr[i].set_xlim(1, 10)
+    axarr[i].set_ylim(0.75, 1.25)
+    axarr[i].text(8, 0.8, r"$z=%.2f$"%zs[i], fontsize=16)
+    axarr[i].set_yticks([0.8, 0.9, 1, 1.1, 1.2])
     axarr[i].tick_params(labelsize=16)
-    axarr[i].text(8, 1.07, r"$z=%.2f$"%zs[i], fontsize=16)
-    axarr[i].set_yticks([0.9, 1, 1.1])
 
 plt.subplots_adjust(hspace=0.1, bottom=0.2, left=0.2)
 fig.add_subplot(111, frameon=False)
